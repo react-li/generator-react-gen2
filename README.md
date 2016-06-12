1dcfd5cb707f859cafb9f89a6f539bf6ef86aa1f
zanjs <root@zanjs.com>
2016-06-12 15:49:37 +0800

    first commit:cat:

---
+++ README.md
@@ -0,0 +1,94 @@
+# Generate a starting point for your react app
+
+The `react-gen` generator is a yeoman generator that creates an app using the same structure detailed in the the [blog post detailing cloning yelp with React](http://fullstackreact.com/articles/react-tutorial-cloning-yelp/).
+
+Using this generator gives you a sane react structure with the following technologies:
+
+* Webpack with hot module reloading (hjs-webpack)
+* PostCSS and CSS modules, including autoprefixer, precss
+* Global CSS loading
+* React / ReactDOM
+* react-router
+* Nested routing with multiple views
+* Testing with karma, mocha, chai
+* React component testing helper enzyme
+* Multiple deployment environments with dotenv configuration
+* Babel with react, stage-0, and es2015
+* font-awesome
+* and more
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-react-gen using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-react-gen
+```
+
+Then generate your new project, answer some questions about your project, and go!
+
+```bash
+yo react-gen
+```
+
+## Workflow
+
+Open your app in a text editor and start to work. To run the application, use the `npm start` script. This will boot a server with hot module reloading:
+
+```bash
+npm run start
+```
+
+To run the tests in our app, we can use the `npm run test` script. This sets up enzyme, boots the tests with karma, and executes them:
+
+```bash
+npm run test
+```
+
+As we're writing tests, sometimes it's just easier to run the tests as we update and edit files. The generator makes this easy using the `npm run test:watch` script. Run this command and then any changes to the files in our project will cause the tests to be run:
+
+```bash
+npm run test:watch
+```
+
+To build the app for distribution, we can use the `npm run build` command:
+
+```bash
+npm run build
+```
+
+## Features
+
+Wanna add redux to the mix? No problem, pass the feature flag of `--redux` when calling generate, i.e.:
+
+```bash
+yo react-gen --redux
+```
+
+The `--redux` flag will also install the [redux-devtools](https://github.com/gaearon/redux-devtools), hidden by default. Pressing the combination of `Ctrl+h` will make them visible.
+
+## Contributing
+
+```shell
+git clone https://github.com/fullstackreact/redux-modules.git
+cd redux-modules
+npm install
+npm start
+```
+___
+
+# Fullstack React Book
+
+<a href="https://fullstackreact.com">
+<img align="right" src="https://github.com/fullstackreact/google-maps-react/raw/master/resources/readme/fullstack-react-hero-book.png" alt="Fullstack React Book" width="155" height="250" />
+</a>
+
+This generator was built alongside the blog post [React Tutorial: Cloning Yelp](https://www.fullstackreact.com/articles/react-tutorial-cloning-yelp/).
+
+This repo was written and is maintained by the [Fullstack React](https://fullstackreact.com) team. In the book, we cover many more projects like this. We walk through each line of code, explain why it's there and how it works.
+
+<div style="clear:both"></div>
+
+## License
+ [MIT](/LICENSE)
---
+++ generators/app/index.js
@@ -0,0 +1,163 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+const tplCopy = function (name, to, props) {
+  this.fs.copyTpl(
+    this.templatePath(name),
+    this.destinationPath(to || name),
+    (props || this.props)
+  );
+};
+
+const fileListCopy = function (files, dest) {
+  const cpy = tplCopy.bind(this);
+  files.forEach(filename => {
+    cpy(filename, dest, this.props);
+  });
+};
+
+const availableFeatures = [
+  'redux'
+];
+
+module.exports = yeoman.Base.extend({
+  prompting: function () {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the gnarly ' + chalk.red('generator-react-gen') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'input',
+      name: 'title',
+      message: 'App name',
+      default: this.appname
+    }, {
+      type: 'input',
+      name: 'authorName',
+      message: 'Author name',
+      default: 'Ari Lerner'
+    }, {
+      type: 'input',
+      name: 'authorEmail',
+      message: 'Author email',
+      default: 'ari@fullstack.io'
+    }];
+
+    // feature options
+    availableFeatures.forEach(feature => this.option(feature));
+
+    return this.prompt(prompts)
+    .then(function (props) {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+
+      this.features = [];
+      availableFeatures.forEach(feature => {
+        if (this.options[feature]) {
+          this.features.push(feature);
+        }
+      });
+    }.bind(this));
+  },
+
+  writing: function () {
+    // root files
+    var copyFiles = fileListCopy.bind(this);
+    var baseFiles = ['package.json',
+      'webpack.config.js',
+      'karma.conf.js',
+      '.babelrc', '.env',
+      'src/'
+    ];
+    var dependencies = [
+      'react', 'react-dom', 'classnames', 'font-awesome',
+      'react-router', 'react-router-redux'
+    ];
+    var devDependencies = [
+      'autoprefixer',
+      'babel-core',
+      'babel-loader',
+      'babel-plugin-transform-es2015-modules-umd',
+      'babel-polyfill',
+      'babel-preset-es2015',
+      'babel-preset-react',
+      'babel-preset-react-hmre',
+      'babel-preset-stage-0',
+      'babel-register',
+      'chai',
+      'chai-enzyme',
+      'cross-env',
+      'css-loader',
+      'cssnano',
+      'dotenv',
+      'enzyme',
+      'expect',
+      'file-loader',
+      'hjs-webpack',
+      'jasmine-core',
+      'json-loader',
+      'karma',
+      'karma-chai',
+      'karma-jasmine',
+      'karma-mocha',
+      'karma-phantomjs-launcher',
+      'karma-sourcemap-loader',
+      'karma-spec-reporter',
+      'karma-webpack',
+      'mocha',
+      'phantomjs-polyfill',
+      'phantomjs-prebuilt',
+      'postcss-loader',
+      'precss',
+      'react-addons-test-utils',
+      'sinon',
+      'style-loader',
+      'url-loader',
+      'webpack'
+    ];
+
+    var featureFiles = {
+      redux: {
+        'src/': {
+          dependencies: ['react-redux',
+            'redux-module-builder', 'redux-thunk'],
+          devDependencies: ['redux-devtools',
+          'redux-devtools-log-monitor', 'redux-devtools-dock-monitor'],
+          files: ['features/redux/']
+        }
+      }
+    };
+
+    copyFiles(baseFiles);
+
+    this.features.forEach(featureKey => {
+      this.log(`======> Enabling ` + chalk.blue(featureKey));
+      const feat = featureFiles[featureKey];
+      Object.keys(feat)
+      .forEach(key => {
+        const dest = key;
+        const feature = feat[key];
+        const files = feature.files;
+        copyFiles(files, dest);
+
+        if (feature.dependencies) {
+          dependencies = dependencies.concat(feature.dependencies);
+        }
+        if (feature.devDependencies) {
+          devDependencies =
+            devDependencies.concat(feature.devDependencies);
+        }
+      });
+    });
+
+    this.npmInstall(dependencies, {save: true});
+    this.npmInstall(devDependencies, {saveDev: true});
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
